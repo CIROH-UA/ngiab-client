6b2f28e55479e47d9951aa01a8fcb21f0869b8de
romer8 <gio.busrom@gmail.com>
2024-05-07 14:46:39 -0600

    needs more research for chunking

--- reactapp/config/webpack.config.js
+++ reactapp/config/webpack.config.js
@@ -29,12 +29,7 @@ module.exports = (env, argv) => {
 			// new WebpackBundleAnalyzer(), // Uncomment to analyze bundle size
 		],
 
-		// externals: ({context, request}, callback) => {
-		// 	if (/xlsx|canvg|pdfmake/.test(request)) {
-		// 		return callback(null, "commonjs " + request);
-		// 	}
-		// 	callback();
-		// },
+
 		module: {
 			rules: [
 				{
@@ -87,41 +82,6 @@ module.exports = (env, argv) => {
 		},
 		optimization: {
 			minimize: true,
-			// minimizer: [new TerserPlugin()],
-			splitChunks: {
-				
-				 minSize: 17000,
-				 minRemainingSize: 0,
-				 minChunks: 2,
-				 maxAsyncRequests: 30,
-				 maxInitialRequests: 30,
-				 automaticNameDelimiter: "_",
-				 enforceSizeThreshold: 30000,
-				//  cacheGroups: {
-				//   common: {
-				//    test: /[\\/]node_modules[\\/]/,
-				//    priority: -5,
-				//    reuseExistingChunk: true,
-				//    chunks: "all",
-				//    name: "common_app",
-				//    minSize: 0,
-				//   },
-				// //   default: {
-				// //    minChunks: 2,
-				// //    priority: -20,
-				// //    reuseExistingChunk: true,
-				// //   },
-				//   // we are opting out of defaultVendors, so rest of the node modules will be part of default cacheGroup
-				// //   defaultVendors: false,
-				// //   reactPackage: {
-				// //    test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
-				// //    name: 'vendor_react',
-				// //    chunks: "all",
-				// //    priority: 10,
-				// //   }
-				//  },
-				
-			   }
 		},
 		devServer: {
 			proxy: {
