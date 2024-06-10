b2aabc149de517206b0e223d94222b8660e1b5ff
romer8 <gio.busrom@gmail.com>
2024-06-09 21:40:02 -0600

    added flowlines

--- reactapp/lib/layersData.js
+++ reactapp/lib/layersData.js
@@ -48,5 +48,30 @@ const sldCatchmentStyle = `<?xml version="1.0" encoding="ISO-8859-1"?>
   </NamedLayer>
 </StyledLayerDescriptor>`
 
-export { initialLayersArray,sldCatchmentStyle }
+const sldFlowPathsStyle = `<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0" 
+    xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd" 
+    xmlns="http://www.opengis.net/sld" 
+    xmlns:ogc="http://www.opengis.net/ogc" 
+    xmlns:xlink="http://www.w3.org/1999/xlink" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <NamedLayer>
+    <Name>nextgen:flowpaths</Name>
+    <UserStyle>
+      <FeatureTypeStyle>
+        <Rule>
+          <LineSymbolizer>
+          <Stroke>
+            <CssParameter name="stroke">#87CEEB</CssParameter>
+            <CssParameter name="stroke-width">2</CssParameter>
+          </Stroke>
+          </LineSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>`
+
+
+export { initialLayersArray,sldCatchmentStyle,sldFlowPathsStyle }
 
