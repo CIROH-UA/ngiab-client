17ec8b4c97bfddb160cb13196c2effe018f46c7e
romer8 <gio.busrom@gmail.com>
2024-11-11 10:07:02 -0700

    changes to use the visualizer with maplibre

--- reactapp/components/mapgl.js
+++ reactapp/components/mapgl.js
@@ -19,16 +19,18 @@ const nexusLayer = {
     },
   };
 
+  
 // Define the layer configuration BaseMap
-  const layerConfig = {
-    id: 'conus-layer',
+  const catchmentLayerConfig = {
+    id: 'catchments-layer',
     type: 'fill',
     source: 'conus',
-    'source-layer': 'conus-layer-name', // Replace with actual source layer name
-    paint: {
-      'fill-color': '#088',
-      'fill-opacity': 0.8,
-    },
+    'source-layer': 'catchments', // Replace with actual source layer name
+    "paint": {
+      "fill-color": ["rgba", 0, 0, 0, 0],
+      "fill-outline-color": ["rgba", 1, 1, 1, 0.5],
+      "fill-opacity": { "stops": [[7, 0], [11, 1]] }
+    }
   };
 
   
@@ -36,19 +38,33 @@ const nexusLayer = {
 const MapComponent = () => {
   const {actions: hydroFabricActions } = useHydroFabricContext(); 
   const [nexusPoints, setNexusPoints] = useState(null);
+  const [selectedCatchmentsConfig, setSelectedCatchmentsConfig] = useState(null);
 
   // PMTiles protocol setup
   useEffect(() => {
     const protocol = new Protocol({ metadata: true });
     maplibregl.addProtocol('pmtiles', protocol.tile);
 
-    appAPI.getNexusGeoJson().then(response => {
-        console.log(response.geojson)
-        setNexusPoints(response.geojson);
+    appAPI.getGeoSpatialData().then(response => {
+        console.log(response)
+        setNexusPoints(response.nexus);
+        const selectedCatchmentLayerConfig = {
+          id: 'selected-catchments-layer',
+          type: 'fill',
+          source: 'conus',
+          'source-layer': 'catchments', // Replace with actual source layer name
+          filter: ['in', 'divide_id', ['literal', response.catchments]],
+          "paint": {
+            "fill-color": ["rgba", 238, 51, 119, 0.316],
+            "fill-outline-color": ["rgba", 238, 51, 119, 0.7],
+            "fill-opacity": { "stops": [[7, 0], [11, 1]] }
+          }
+        };
+        setSelectedCatchmentsConfig(selectedCatchmentLayerConfig);
+
     }).catch(error => {
         console.log(error)
-    })
-
+    });
     return () => {
       maplibregl.removeProtocol('pmtiles');
     };
@@ -66,30 +82,36 @@ const handleMapClick = (event) => {
     
     // Include both 'conus-layer' and 'nexus-layer' in the layers array
     const features = map.queryRenderedFeatures(event.point, {
-      layers: ['conus-layer', 'nexus-layer'],
+      layers: ['catchments-layer', 'nexus-layer', 'selected-catchments-layer'],
     });
   
     if (features.length > 0) {
       // Loop through all features at the click point
       for (const feature of features) {
         const layerId = feature.layer.id;
+        console.log('Clicked on layer:', layerId);
         console.log('Feature properties:', feature.properties);
-        if (layerId === 'conus-layer') {
+        if (layerId === 'catchments-layer') {
           // Handle click on 'conus-layer'
-          console.log('Clicked on conus-layer');
+          console.log('Clicked on catchments-layer');
+          // map.setFilter('catchments', ['any', ['in', 'divide_id', ['literal', catchments]]])
           // Implement interaction logic specific to 'conus-layer' here
+          hydroFabricActions.reset_teehr();
+          hydroFabricActions.set_catchment_id(feature.properties.divide_id);
         } else if (layerId === 'nexus-layer') {
           // Handle click on 'nexus-layer'
           console.log('Clicked on nexus-layer');
           hydroFabricActions.reset_teehr();
           var nexus_id = feature.properties.id;
-          
-
           hydroFabricActions.set_nexus_id(nexus_id);
           if (feature.ngen_usgs != "none"){
             hydroFabricActions.set_teehr_id(feature.properties.ngen_usgs);
           }
         }
+        else if (layerId === 'selected-catchments-layer'){
+          console.log('Clicked on selected-catchments-layer');
+        }
+
       }
     }
   };
@@ -108,7 +130,8 @@ const handleMapClick = (event) => {
       {/* Add the PMTiles source */}
       <Source id="conus" type="vector" url={pmtilesUrl}>
         {/* Add the layer that uses the source */}
-        <Layer {...layerConfig} />
+        <Layer {...catchmentLayerConfig} />
+        <Layer {...selectedCatchmentsConfig} />
       </Source>
       {nexusPoints && (
         <Source id="nexus-points" type="geojson" data={nexusPoints}>
--- reactapp/services/api/app.js
+++ reactapp/services/api/app.js
@@ -3,9 +3,6 @@ import apiClient from "services/api/client";
 const APP_ROOT_URL = process.env.TETHYS_APP_ROOT_URL;
 
 const appAPI = {
-    getNexusGeoJson: () => {
-        return apiClient.get(`${APP_ROOT_URL}getNexuslayer/`);
-    },
     getNexusTimeSeries: (params) => {
         return apiClient.get(`${APP_ROOT_URL}getNexusTimeSeries/`, { params });
     },
@@ -23,7 +20,11 @@ const appAPI = {
     },
     getTeehrVariables: (params) => {
         return apiClient.get(`${APP_ROOT_URL}getTeehrVariables/`, { params });
+    },
+    getGeoSpatialData: () => {
+        return apiClient.get(`${APP_ROOT_URL}getGeoSpatialData/`);
     }
+
 
 }
  
