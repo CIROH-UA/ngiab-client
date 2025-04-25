94b158c8655b29d6160f274fbfb4c736c79b9169
romer8 <gio.rombus@gmail.com>
2025-04-24 19:51:16 -0600

    added one more level

--- reactapp/features/DataStream/components/bucketNamesSelect.js
+++ reactapp/features/DataStream/components/bucketNamesSelect.js
@@ -1,28 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { use } from 'react';
+import React, { useState, useEffect, Fragment  } from 'react';
+
 import { Form, Button, Alert, Spinner, Stack } from 'react-bootstrap';
 import appAPI from 'services/api/app';
+import SelectComponent from './selectComponent';
+
+
+
 
 export default function BucketNamesSelect() {
+  const [datesBucket, setDatesBucket] = useState([]);
+  const [availableForecastList, setAvailableForecastList] = useState([]);
   const [name, setName] = useState('');
-  const [url,  setUrl]  = useState('');
+  
   const [loading, setLoading] = useState(false);
   const [error,   setError]   = useState(null);
   const [success, setSuccess] = useState(false);
 
 
+  const handleChangeDate = (e) => {
+    console.log('Selected date:', e);
+    const params = {
+      avail_date: e[0].value,
+    }
+    appAPI.getDataStreamNgiabAvailableForecast(params)
+    .then((data) => {
+      // console.log('Success', data);
+        console.log('Success', data);
+        setAvailableForecastList(data.ngen_forecast);
+          if (data.error) {
+            // toast.error("Error fetching Model Run Data", { autoClose: 1000 });
+            return;
+          }
+  
+          // toast.success("Successfully retrieved Model Run Data", { autoClose: 1000 });
+        })
+        .catch((error) => {
+          console.error('Failed', error);
+        });
+
+  }
+
+  const handleChangeForecast = (e) => {
+    console.log('Selected forecast:', e);
 
 
-  useEffect(() => {
-    if (success) {
-      const params = {
-        model_run_name: name.trim(),
-        model_run_s3_path: url.trim(),   // keep raw
-      };
-    
-      appAPI.importModelRuns(params)
+  }
+
+  useEffect(() => {    
+      appAPI.getDataStreamNgiabDates()
         .then((data) => {
+          // console.log('Success', data);
             console.log('Success', data);
+            setDatesBucket(data.ngen_dates);
               if (data.error) {
                 // toast.error("Error fetching Model Run Data", { autoClose: 1000 });
                 return;
@@ -33,10 +62,27 @@ export default function BucketNamesSelect() {
             .catch((error) => {
               console.error('Failed', error);
             });
-    }
-  }, [success]);
+  }, []);
 
   return (
-    <></>
+    <Fragment>
+    {datesBucket.length > 0 &&
+        <Fragment>
+            <SelectComponent 
+              optionsList={datesBucket} 
+              onChangeHandler={handleChangeDate}
+            />
+        </Fragment>
+    }
+    {availableForecastList.length > 0 &&
+        <Fragment>
+            <SelectComponent 
+              optionsList={availableForecastList} 
+              onChangeHandler={handleChangeForecast}
+            />
+        </Fragment>
+    }
+
+    </Fragment>
   );
 }
