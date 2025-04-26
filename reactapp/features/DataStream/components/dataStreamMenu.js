25afa9dcc8ff2c0612f8f8a5f502d773d2b4cfd8
romer8 <gio.rombus@gmail.com>
2025-04-25 18:37:08 -0600

    more cleaning

--- reactapp/features/DataStream/components/dataStreamMenu.js
+++ reactapp/features/DataStream/components/dataStreamMenu.js
@@ -1,38 +1,14 @@
 import React, { Fragment, useState } from 'react';
 import styled from 'styled-components';
 import BucketNamesSelect from './bucketNamesSelect';
-const Container = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100%;
-  background-color: #4f5b67;
-
-  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
-  z-index: 1000;
-`;
 
 
-
-// Content inside the panel.
-const Content = styled.div`
-  padding: 16px;
-  margin-top: 100px;
-`;
-
-const DataStreamMenu = ({
-    isVisible
-}) => {
+const DataStreamMenu = () => {
   
   return (
-    <Fragment>    
-      <Container isVisible={isVisible}>
-        <Content>
+    <Fragment>
           <h3>NGIAB DataStream S3 Data</h3>
           <BucketNamesSelect />
-        </Content>
-      </Container>
     </Fragment>
 
   );
