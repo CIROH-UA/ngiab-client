090dcade7d572b306d88b0798a27c4293a40d433
romer8 <gio.rombus@gmail.com>
2025-12-09 14:45:54 -0700

    Refactor: Remove old hydroFabric and dataStream features
    - Deleted obsolete hooks, providers, actions, and reducers related to hydroFabric and dataStream.
    - Removed associated components and views that are no longer in use.
    - Cleaned up unused context and action types to streamline the codebase.

--- reactapp/old/features/ModelRuns/hooks/useModelRunsContext.js
+++
@@ -1,7 +0,0 @@
-import { useContext} from 'react';
-import ModelRunsContext from '../contexts/modelRunsContext';
-
-
-export const useModelRunsContext = () => {
-    return useContext(ModelRunsContext)
-}
--- reactapp/old/hooks/useDataStreamContext.js
+++
@@ -1,7 +0,0 @@
-// import { useContext} from 'react';
-// import DataStreamContext from '../old/contexts/dataStreamContext';
-
-
-// export const useDataStreamContext = () => {
-//     return useContext(DataStreamContext)
-// }
